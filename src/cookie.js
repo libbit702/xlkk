ea3577d679c0be4a9654126f0c7e3d302e44ec50
libbit702 <lee_dlut@hotmail.com>
2013-03-22 11:26:19 +0800

    cookie添加注释

--- src/cookie.js
+++ src/cookie.js
@@ -1,7 +1,20 @@
+/**
+ * 通用操作cookie的方法包装
+ *
+ * @module cookie
+ */
 define(function(){
     var Cookie = {
         host : window.location.host
     };
+    
+    /**
+     * 通过cookie键值取对应的value
+     *
+     * @method getCookie
+     * @param {String} name
+     * @return String or empty string("") if not found
+     */
     Cookie.getCookie = function( name ) {
         try{
             var str = (document.cookie.match(new RegExp("(^"+name+"| "+name+")=([^;]*)")) == null) ? "" : decodeURIComponent( RegExp.$2 );
@@ -14,7 +27,15 @@ define(function(){
             return "";
         }
     };
-            
+     
+    /**
+     * 设置当前url域下的cookie
+     *
+     * @method setCookie
+     * @param {String} name
+     * @param {String} value
+     * @param {Int} hours
+     */
     Cookie.setCookie = function( name,value,hours ) {        
         if ( arguments.length > 2 ) {
             var expireDate = new Date(new Date().getTime() + hours * 3600000);
@@ -23,7 +44,13 @@ define(function(){
             document.cookie = name + "=" + encodeURIComponent( value ) + "; path=/; domain=" + this.host;
         }
     };
-
+    
+    /**
+     * 设置当前文档的domain域
+     *
+     * @method setDomain
+     * @param {String} domain
+     */
     Cookie.setDomain = function(domain){
         document.domain = domain;
     };

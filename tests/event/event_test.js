26bbdee71eb5f1e4ac71799a73680094b63a2f92
libbit702 <lee_dlut@hotmail.com>
2013-07-21 17:14:06 +0800

    增加eventutil测试用例

--- tests/event/event_test.js
+++ tests/event/event_test.js
@@ -4,10 +4,11 @@ require.config({
 });
 
 require(["eventutil"], function(e) {
-    test('事件绑定测试', 1, function(){
+    test('事件绑定测试', 2, function(){
         var aid = document.getElementById('lnk1');
         e.addEventHandler(aid, 'click', function(){
             ok(true, aid.id+'触发点击了');
+            deepEqual(this, aid, '$this is equal to event binded object');
         });
         aid.click();
     });
